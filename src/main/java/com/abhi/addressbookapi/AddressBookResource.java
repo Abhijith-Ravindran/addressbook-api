6f5723ce1598388a0e8b6cd148b6929d1cc1ce63
Abhijith-Ravindran <abhijith.professional@gmail.com>
2021-01-19 18:54:42 +0530

    Initial commit

---
+++ src/main/java/com/abhi/addressbookapi/AddressBookResource.java
@@ -0,0 +1,31 @@
+package com.abhi.addressbookapi;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+@RestController
+@RequestMapping("/api")
+public class AddressBookResource {
+
+    private ConcurrentMap<String,Contact> contacts = new ConcurrentHashMap<>();
+
+    @GetMapping("/{id}")
+    public Contact getContact(@PathVariable String id){
+        return contacts.get(id);
+    }
+
+    @GetMapping("/")
+    public List getAllContacts(){
+        return new ArrayList<Contact>(contacts.values());
+    }
+
+    @PostMapping("/")
+    public Contact addContact(@RequestBody Contact contact){
+        contacts.put(contact.getId(), contact);
+        return contact;
+    }
+}
